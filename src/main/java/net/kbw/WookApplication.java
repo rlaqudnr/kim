f8806ad10d0dace605085980ba969031e92e27fd
DESKTOP-N0BUQNT\HOME <rlaqudnr@naver.com>
2023-02-07 14:20:09 +0900

    app

--- src/main/java/net/kbw/WookApplication.java
+++ src/main/java/net/kbw/WookApplication.java
@@ -2,14 +2,30 @@ package net.kbw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WookApplication {
+public class WookApplication  extends SpringBootServletInitializer{
+	
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(WookApplication.class);
+		
+		//
+		
+	}
 
 	public static void main(String[] args) {
 		
 		//
 		SpringApplication.run(WookApplication.class, args);
+		
+		
 	}
 
 }
